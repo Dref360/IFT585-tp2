bf3334ec63e7adc49a766ed5e73a189dbcd46ecb
Bruno Cadorette <brunocadorette@outlook.com>
2015-06-12 17:34:27 -0400

    LS

--- RouterNetwork/Algorythm/LS.cs
+++ RouterNetwork/Algorythm/LS.cs
@@ -6,15 +6,60 @@ using System.Threading.Tasks;
 
 namespace RouterNetwork
 {
-
     class LS : IRoutingAlgorithm
     {
-        
-        private List<int> nodes;
-        private Dictionary<int, int> cost; 
+        private class Node : IComparable<Node>
+        {
+            public Guid RouterId { get; set; }
+            public Guid OldRoute { get; set; }
+            public int Cost { get; set; }
+            public override int GetHashCode()
+            {
+                return RouterId.GetHashCode();
+            }
+
+            public int CompareTo(Node other)
+            {
+                return Cost.CompareTo(other.Cost);
+            }
+            public Node BuildPath(Node other)
+            {
+                int newCost = other.Cost + Cost(this.RouterId, other.RouterId);
+                if (IsAdjacent(this.RouterId,other.RouterId) && newCost < this.Cost)
+                {
+                    return new Node()
+                    {
+                        RouterId = RouterId,
+                        OldRoute = other.RouterId,
+                        Cost = newCost
+                    };
+                }
+                else
+                {
+                    return this;
+                }
+
+            }
+        }
+        IEnumerable<Guid> Graph();
+        static bool IsAdjacent(Guid a, Guid b);
+        static int Cost(Guid a, Guid b);
         public void UpdateRoute()
         {
-            
+            var processedNodes = new HashSet<Node>();
+            var nodes = new Guid();
+            var nodesToProcess = Graph().Where(x => x != nodes).Select(p => new Node()
+                {
+                    RouterId = p,
+                    Cost = IsAdjacent(nodes, p) ? Cost(nodes, p) : int.MaxValue
+                });
+            int numberOfNodes = nodesToProcess.Count();
+            while (numberOfNodes > processedNodes.Count)
+            {
+                var w = nodesToProcess.Where(n => !processedNodes.Contains(n)).Min();
+                processedNodes.Add(w);
+                nodesToProcess = nodesToProcess.Where(v => v != w).Select(w.BuildPath);
+            }
         }
     }
 }
