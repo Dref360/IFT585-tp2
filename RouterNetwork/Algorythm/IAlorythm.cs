eda0ca791e91173f2ffbef0a77f591b11290c046
Dref360 <frederic.branchaud.charron@gmail.com>
2015-06-12 11:08:43 -0400

    First Commit

---
+++ RouterNetwork/Algorythm/IAlorythm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouterNetwork
+{
+    //DS or LS
+    interface IAlgorythm
+    {
+        void UpdateRoute();
+    }
+}
---
+++ RouterNetwork/CostConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouterNetwork
+{
+    static class CostConfiguration
+    {
+        public static int Cost(int port1, int port2)
+        {
+            return 0;
+        }
+    }
+}
